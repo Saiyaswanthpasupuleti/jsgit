896ba7b5af3503f3dadde354f33ed150657c7430
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-08-15 12:04:59 +0530

    d

---
+++ 14-aug.js
@@ -0,0 +1,48 @@
+// tasks:
+// ------
+// 1.Uppercase first letter
+
+// - Input: "hello"
+// - Output: "Hello"
+
+// - Input: "world"
+// - Output: "World"
+// hint:charAt(), toUpperCase(), slice(), concat()
+
+// 2.Remove vowels
+
+// - Input: "hello"
+// - Output: "hll"
+
+// - Input: "world"
+// - Output: "wrld"
+
+// using replace().
+
+// research required here
+
+// let a="hello";
+// let b=a.charAt(0);
+// let c=b.toUpperCase();
+// let d=a.slice(1);
+// console.log(c+d)
+
+
+
+// let user="hello";
+// var b=""
+// for(let i in user){
+//     b+=user[i]
+// }
+// if(b=user.replace(/[aeiou]/g,"")){
+//     console.log(b)
+// }
+
+
+
+// let user="world";
+// let b=user;
+// if(b=user.replace(/[a,e,i,o,u]/g,"")){
+//     console.log(b)
+// }
+
