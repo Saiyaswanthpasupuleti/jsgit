8b7b9c987eb66b21271cf918bc5b12c5e43ee04f
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-08-15 11:52:10 +0530

    new

---
+++ 13-aug.js
@@ -0,0 +1,41 @@
+// TASK: 
+
+// doc on scopes,functions,hoisting.
+// problems on string
+// 1.check palindrome or not?
+// 2.i/p-- somEThinG
+//   o/p--SOMetHINg
+
+
+// function sai(user){
+//     let b=""
+//     for(let i=user.length-1;i>=0;i--){
+//         b=b+user[i]  
+//     }
+//     console.log("the actual string is:",user)
+//     console.log("the reversed string is ",b)
+//     if(b==user){
+//         console.log("so it is a palindrome")
+//     }
+//     else{
+//         console.log("it is not a palindrome")
+//     }
+// }
+// sai("mom")
+
+
+
+// let user="somEThinG";
+// for(let n of user){  
+// if(user=n.replace(/[a-z]/g,"")){
+//     console.log(user.toLowerCase())
+// }
+// else if(user=n.replace(/[A-Z]/g,"")){
+//     console.log(user.toUpperCase())
+// }
+// }
+
+
+//google drive link for the document
+//=>
+// https://drive.google.com/drive/folders/12dxsyXhMtE9Js_ctOi2YrOGXlRHTtuCu
