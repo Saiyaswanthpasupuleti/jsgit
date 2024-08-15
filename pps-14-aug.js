27aef2531b716e10032b07684b7a8c20fe4ca149
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-08-15 12:40:22 +0530

    d

---
+++ pps-14-aug.js
@@ -0,0 +1,101 @@
+// 1) WAP to print sum of non primes in a number
+
+// input: Enter a number: 763328
+// output: 14
+
+
+// let user=763328;
+// let a=user.toString();
+// let sum=0;
+// for(let c of a){
+//     var prime=true;
+//     if(c>1){
+//         for(let i=2;i<c;i++){
+//             if(c%i==0){
+//                 sum=sum+Number(c);
+//                 prime=false;
+//                 break;    
+//             }
+//         }
+//         if(prime==true){
+            
+//         }
+//     }
+//     else{
+//         console.log("enter a valid nnumber")
+//     }
+// }
+// console.log("the sum of non-prime numbers are:",sum)
+
+
+
+
+// 3) WAP to check whether prime sum is bigger or non prime sum is bigger
+
+// input: Enter a number: 163379
+// output: Non prime sum is bigger.
+
+// input: Enter a number: 2045
+// output: Prime sum is bigger.
+
+
+// let user=163379;
+// let a=user.toString();
+// let sum=0;
+// let su=0;
+// for(let b of a){
+//     var prime=true;
+//     if(a>1){
+//         for(let i=2;i<b;i++){
+//             if(b%i==0){
+//                 sum=sum+Number(b);
+//                 prime=false;
+//                 break;
+//             }
+//         }
+//         if(prime==true){
+//             su=su+Number(b)
+//         }
+//     }
+//     else{
+//         console.log("enter a valid number")
+//     }
+// }
+// console.log("the sum of non prime numbers is:",sum)
+// console.log("the sum of prime numbers is:",su)
+// if(sum>su){
+//     console.log("non prime has greater number")
+// }
+// else{
+//     console.log("prime has greater number")
+// }
+
+
+// 2) WAP to check whether a number is prime or not by using function
+
+// input: Enter a number: 7
+// output: 7 is prime
+
+// function sai(user){
+// let b=user.toString();
+// var prime=true;
+// if(user>1){
+//     for(let i=2;i<b;i++){
+//         if(b%i==0){
+//         console.log("the given number is not a prime")
+//         prime=false;
+//         break;
+//         }
+
+//     }
+//     if(prime==true){
+//         console.log("the given number is a prime number")
+//     }
+// }
+// else{
+//     console.log("1 is neither composite nor a prime")
+// }
+// return "end"
+// }
+// console.log(sai(7));
+
