4172c855b357642af4a5f5a023ad9d5cfca92fb5
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-08-20 13:03:37 +0530

    new

---
+++ 17-aug.js
@@ -0,0 +1,83 @@
+// 1) WAP to print the nth Fibonacci number based on the user input:
+
+// input: Enter fib position: 7
+// output: 8
+// let user=7;
+// let a=0;
+// let b=1;
+// for(let i=1;i<=user-1;i++){
+//     var c=a+b;
+//     a=b;
+//     b=c;
+   
+// }
+// console.log(a);
+
+
+
+// 2) WAP to print the reverse of a number
+
+// Test Case-1
+// input: Enter a number: 872
+// output: 278
+
+// Test Case-2
+// input: Enter a number: 240
+// output: 42
+
+// Test Case-3
+// input: Enter a number: -920
+// output: -29
+
+// let user=-340;
+// let a=user.toString();
+// let res="";
+// if(a[0]=="-"){
+//     for(let i=a.length-1;i>=1;i--){
+//         res+=a[i]
+       
+//     }
+//     console.log("-",Number(res))
+   
+// }
+
+// else{
+// for(let i=a.length-1;i>=0;i--){
+//     console.log(a[i])
+// }
+// }
+
+
+
+
+
+
+
+// 3) WAP to print the Fibonacci series based on the user input
+
+// Test Case-1
+// input: Enter number: 5
+// output: 0 1 1 2 3 5
+
+// Test Case-2
+// input: Enter a number: 20
+// output: 0 1 1 2 3 5 8 13
+
+// let user=5;
+// let a=0;
+// let b=1;
+// let res=""
+// for(let i=1;i<=user;i++){
+//     res+=a+""
+  
+//     if(b<user){
+//         var c=a+b;
+//         a=b;
+//         b=c
+//     }
+// }
+// console.log(res)
+
+
+
+
--- pps-14-aug.js
+++ pps-14-aug.js
@@ -99,3 +99,6 @@
 // }
 // console.log(sai(7));
 
+
+
+
